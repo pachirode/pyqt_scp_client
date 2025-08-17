16710c26dc20239afde70a6d1754ecd37e3e0d0a
Rodepachi <pachirode@gmail.com>
2025-08-17 21:24:07 +0800

    实现基本界面

---
+++ app.py
@@ -0,0 +1,128 @@
+import glob
+import os.path
+import shutil
+import sys
+import time
+
+import yaml
+from PyQt5.QtCore import QProcess
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QWidget, QApplication, QDialog, QMessageBox, QGridLayout, QPushButton, QFileDialog
+
+from deepdiff import DeepDiff
+
+from consts import CONFIG_FILE, LANGUAGE
+from ssh import Ui_SSH
+from sshconfig import Ui_SSHConfig
+
+
+class SSHConfig(QDialog):
+    def __init__(self, parent, data):
+        super(SSHConfig, self).__init__(parent)
+
+        self.ui = Ui_SSHConfig()
+        self.ui.setupUi(self)
+        self.ui.Remote_ip_edit.setText(data.get("ip"))
+        self.ui.Remote_dir_edit.setText(data.get("remote_dir"))
+        self.ui.local_dir_edit.setText(data.get("local_dir"))
+
+        self.ui.openDirButton.clicked.connect(self.open_dir)
+
+    def open_dir(self):
+        directory = QFileDialog.getExistingDirectory(self, "Select Directory")
+        self.ui.local_dir_edit.setText(directory)
+
+    def as_dict(self):
+        return {
+            "ip": self.ui.Remote_ip_edit.text(),
+            "remote_dir": self.ui.Remote_dir_edit.text(),
+            "local_dir": self.ui.local_dir_edit.text()
+        }
+
+
+class SSH(QWidget):
+    def __init__(self, name, data):
+        super(SSH, self).__init__()
+
+        self.ui = Ui_SSH()
+        self.ui.setupUi(self)
+
+        self.ssh_config = SSHConfig(self, data)
+        self.ssh_config.setWindowTitle(name)
+        self.ssh_config.setModal(True)
+        self.ui.name.setText(name)
+
+        self.ssh_config.icon = f"./icons/{name}.svg"
+
+        if not os.path.exists(self.ssh_config.icon):
+            self.ssh_config.icon = "./icons/debain.svg"
+
+        self.ui.icon.setPixmap(QPixmap(self.ssh_config.icon))
+        self.ui.action_upload.clicked.connect(self.upload)
+        self.ui.action_download.clicked.connect(self.download)
+        self.ui.action_settings.clicked.connect(self.ssh_config.show)
+
+        self.process = None
+
+    def upload(self):
+        self.process = QProcess()
+
+
+    def download(self):
+        pass
+
+
+class SSHManager(QWidget):
+    def __init__(self):
+        super().__init__()
+        with open(CONFIG_FILE, "r") as fp:
+            self.data = yaml.load(fp, Loader=yaml.FullLoader)
+
+        self.grid = QGridLayout(self)
+        self.servers = []
+
+        i = 0
+        for i, server in enumerate(self.data["servers"]):
+            server = SSH(server, self.data["servers"][server])
+            self.servers.append(server)
+            self.grid.addWidget(server, i, 0)
+
+        self.close = QPushButton("Close")
+        self.grid.addWidget(self.close, i + 1, 1)
+
+        self.setLayout(self.grid)
+        self.resize(10, 10)
+        self.setWindowTitle(LANGUAGE.Title)
+        self.show()
+
+    def closeEvent(self, event):
+        data = {"servers": {}}
+        for server in self.servers:
+            data["servers"][server.ui.name.text()] = server.ssh_config.as_dict()
+        changed = DeepDiff(self.data, data, ignore_order=True)
+
+        if changed:
+            timestamp = int(time.time())
+            shutil.copy(CONFIG_FILE, F"./history/{CONFIG_FILE}-{timestamp}")
+            with open(CONFIG_FILE, "w") as fp:
+                yaml.dump(data, fp)
+            backup_configs = glob.glob(f"{CONFIG_FILE}-*")
+            if len(backup_configs) > 10:
+                for config in sorted(backup_configs, reverse=True)[10:]:
+                    os.remove(config)
+        event.accept()
+
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+
+    if not os.path.exists(CONFIG_FILE):
+        mb = QMessageBox()
+        mb.setText(LANGUAGE.CONFIG_NOT_FOUND)
+        mb.setWindowTitle(LANGUAGE.OOPS)
+        mb.setStandardButtons(QMessageBox.Close)
+        mb.show()
+    else:
+        sm = SSHManager()
+    sys.exit(app.exec_())
---
+++ consts.py
@@ -0,0 +1,30 @@
+CONFIG_FILE = 'config.yaml'
+
+
+class LANGUAGE:
+    Title = 'SSH Files'
+    Upload = 'Upload'
+    Download = 'Download'
+    Open = 'Open'
+    CONFIG_NOT_FOUND = 'Config file not found'
+    OOPS = 'Oops!'
+
+
+class HOSTS:
+    REMOTE_ADDRESS = 'remote_address'
+    REMOTE_PORT = 'remote_port'
+
+
+class CMDS:
+    SSH = "ssh"
+    SSH_KILL_NIX = "killall ssh"
+    SSH_KILL_WIN = "taskkill /im ssh.exe /t /f"
+    SCP = "scp"
+    SCP_UPLOAD_TEMPLATE = "scp -r {} {}:{}"
+    SCP_DOWNLOAD_TEMPLATE = "scp -r {}:{} {}"
+
+
+class ICONS:
+    UPLOAD = ':/icons/upload.png'
+    DOWNLOAD = ':/icons/download.png'
+    OPEN = ':/icons/file-open.png'
